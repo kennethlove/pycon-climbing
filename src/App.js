72f35f5a742d2f078e3a4542a585dbb41b0a3eb1
Kenneth Love <kennethlove@gmail.com>
2018-05-12 11:54:51 -0400

    basics

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { GoogleMap, Marker, withGoogleMap, withScriptjs } from "react-google-maps"
+import logo from './logo.svg';
+import './App.css';
+
+const climbClevelandLat = 41.48198;
+const climbClevelandLong = -81.6895357;
+
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap defaultZoom={17} defaultCenter={{ lat: climbClevelandLat, lng: climbClevelandLong }}>
+    {props.isMarkerShown && <Marker position={{ lat: climbClevelandLat, lng: climbClevelandLong }} />}
+  </GoogleMap>
+));
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Let's go climbing at PyCon!</h1>
+          <address>Climb Cleveland, 2190 Professor St, Cleveland, OH 44113</address>
+          <p>Sunday, May 13th, 5pm &ndash; 8pm</p>
+          <p>Cost is <strong>$14</strong> for the day pass and <strong>$4</strong> for shoes.</p>
+          < p > You can fill out the waiver online <a href="http://climb-cleveland.com/digital-waiver/">here</a>.</p>
+        </header>
+        <MyMapComponent
+          isMarkerShown
+          googleMapURL = "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement = { < div style = {
+              {
+                height: `100%`
+              }
+            }
+            />}
+            containerElement = { < div style = {
+                {
+                  height: `400px`
+                }
+              }
+              />}
+              mapElement = { < div style = {
+                  {
+                    height: `100%`
+                  }
+                }
+                />} /
+                >
+      </div>
+    );
+  }
+}
+
+export default App;
